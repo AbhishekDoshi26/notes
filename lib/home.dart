462f9c5d48aa2a3b83a2f68eb4059290c629c588
Abhishek Doshi <adoshi26.ad@gmail.com>
2024-04-05 19:48:41 +0530

    first commit

---
+++ lib/home.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Notes App'),
+      ),
+      body: GridView.count(
+        crossAxisCount: 2,
+        children: List.generate(
+          100,
+          (index) => const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Card(
+              color: Colors.red,
+              elevation: 5,
+              shadowColor: Colors.white,
+            ),
+          ),
+        ).toList(),
+      ),
+    );
+  }
+}
