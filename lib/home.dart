be5a3b1fe91ce9983afa13b9776cec99699e23d9
Abhishek Doshi <adoshi26.ad@gmail.com>
2024-04-06 13:40:34 +0530

    feat: completed day2

--- lib/home.dart
+++ lib/home.dart
@@ -1,26 +1,69 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({
     super.key,
   });
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final TextEditingController _controller = TextEditingController();
+  final List<String> data = [];
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (context) {
+              return Scaffold(
+                body: Column(
+                  children: [
+                    TextField(
+                      controller: _controller,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          data.add(_controller.text);
+                          _controller.clear();
+                          Navigator.pop(context);
+                        });
+                      },
+                      child: const Text('Submit'),
+                    ),
+                  ],
+                ),
+              );
+            },
+          );
+        },
+        child: const Icon(Icons.add),
+      ),
       appBar: AppBar(
         title: const Text('Notes App'),
       ),
       body: GridView.count(
         crossAxisCount: 2,
         children: List.generate(
-          100,
-          (index) => const Padding(
-            padding: EdgeInsets.all(8.0),
+          data.length,
+          (index) => Padding(
+            padding: const EdgeInsets.all(8.0),
             child: Card(
               color: Colors.red,
               elevation: 5,
               shadowColor: Colors.white,
+              child: Text(
+                data[index],
+                style: const TextStyle(
+                  fontSize: 20,
+                ),
+              ),
             ),
           ),
         ).toList(),
